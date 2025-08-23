43042993841291be125d78df93136e23999ec4a0
Terry Ott <Terry.Ott@gmail.com>
2025-08-23 20:39:32 +0000

    Updated to per drive model

--- compute_afr.py
+++ compute_afr.py
@@ -10,32 +10,12 @@ import re
 def _parse_args() -> argparse.Namespace:
     arg_parser: argparse.ArgumentParser = argparse.ArgumentParser(
         description='Compute annualized failure rate ("AFR")')
-    arg_parser.add_argument('drive_model_families_json',
-                            help='JSON file with drive model family mappings')
     arg_parser.add_argument('drive_stats_csv', help='CSV file with drive statistics')
     arg_parser.add_argument('computed_afr_csv', help='CSV file with computed daily AFR')
 
     args: argparse.Namespace = arg_parser.parse_args()
 
     return args
-
-
-def _generate_regex_map(args: argparse.Namespace) -> list[dict[str, str]]:
-    with open(args.drive_model_families_json, 'r') as json_handle:
-        json_dict: dict[str, str] = json.load(json_handle)
-
-    drive_model_mappings: list[dict[str, str]] = []
-
-    for drive_model_family in json_dict:
-        for drive_model_regex in json_dict[drive_model_family]:
-            drive_model_mappings.append(
-                {
-                    'regex'                 : drive_model_regex,
-                    'drive_model_family'    : drive_model_family,
-                }
-            )
-
-    return drive_model_mappings
 
 
 def _clean_drive_model_str(drive_model_str: str) -> str:
@@ -47,9 +27,7 @@ def _clean_drive_model_str(drive_model_str: str) -> str:
     return clean_str
 
 
-def _parse_csv_data(args: argparse.Namespace,
-                    drive_model_family_mappings: list[dict[str, str]]
-                    ) -> dict[str, dict[str, dict[str, int]]]:
+def _parse_csv_data(args: argparse.Namespace) -> dict[str, dict[str, dict[str, int]]]:
 
     parsed_data: dict[str, dict[str, dict[str, int]]] = {}
     with open(args.drive_stats_csv, 'r') as csv_handle:
@@ -58,54 +36,29 @@ def _parse_csv_data(args: argparse.Namespace,
         # Skip header row
         next(csv_reader)
 
-        # Read data rows
-        current_model:str | None = None
-        current_model_family:str | None = None
-
         for curr_csv_row in csv_reader:
             drive_model: str = _clean_drive_model_str(curr_csv_row[0])
             #print(f"Drive model: {drive_model}")
+
+            if drive_model not in parsed_data:
+                parsed_data[drive_model]:dict[str, dict[str, int]] = {}
+
             curr_date: str = curr_csv_row[1]
 
-            # Do we need to need to deal with a model change
-            if drive_model != current_model:
-                # Reset model family, it's a new drive model
-                current_model = drive_model
-                current_model_family = None
-
-                # Walk it over all drive model -> drive model family regexes to see if we got a match
-                for current_mapping in drive_model_family_mappings:
-                    #print(f"\tTrying match: {current_mapping['regex']}")
-                    if re.search(current_mapping['regex'], drive_model):
-                        # print(f"{drive_model} -> {current_mapping['drive_model_family']} "
-                        #       f"(regex: {current_mapping['regex']})")
-
-                        current_model_family = current_mapping['drive_model_family']
-
-                        if current_model_family not in parsed_data:
-                            parsed_data[current_model_family] = {}
-                            # print(f"Added {current_model_family} to parsed data")
-
-            # If we don't have a drive model family for this drive by the time we get here,
-            #       we don't care about the drive model
-            if current_model_family is None:
-                # Skip this drive
-                continue
-
-            if curr_date not in parsed_data[current_model_family]:
-                parsed_data[current_model_family][curr_date] = {
+            if curr_date not in parsed_data[drive_model]:
+                parsed_data[drive_model][curr_date] = {
                     'drive_count': 0,
                     'failure_count': 0,
                 }
 
-            parsed_data[current_model_family][curr_date]['drive_count'] += int(curr_csv_row[2])
-            parsed_data[current_model_family][curr_date]['failure_count'] += int(curr_csv_row[3])
+            parsed_data[drive_model][curr_date]['drive_count'] += int(curr_csv_row[2])
+            parsed_data[drive_model][curr_date]['failure_count'] += int(curr_csv_row[3])
 
     return parsed_data
 
 
-def _prep_csv_data_for_afr_calc(
-        parsed_csv_data: dict[str, dict[str, dict[str, int]]] ) -> dict[str, list[dict[str, int | str]]]:
+def _prep_csv_data_for_afr_calc(parsed_csv_data: dict[str, dict[str, dict[str, int]]]
+                                ) -> dict[str, list[dict[str, int | str]]]:
     """Take CSV data and move it to a list of dicts, with day index and cumulative stats. """
 
     #print("Prepping data")
@@ -147,15 +100,15 @@ def _prep_csv_data_for_afr_calc(
     return afr_prepped_data
 
 
-def _compute_afr_per_drive_model_family(
+def _compute_afr_per_drive_model(
         afr_calc_prepped_data: dict[str, list[dict[str, str | int | float]]]):
 
     computed_afr_data: dict[str, list[dict[str, str | int | float]]] = {}
 
-    for curr_model_family_name in afr_calc_prepped_data:
-        computed_afr_data[ curr_model_family_name ] = []
+    for curr_drive_model_name in afr_calc_prepped_data:
+        computed_afr_data[ curr_drive_model_name ]: list[dict[str, str | int | float]] = []
 
-        for curr_datapoint in afr_calc_prepped_data[curr_model_family_name]:
+        for curr_datapoint in afr_calc_prepped_data[ curr_drive_model_name ]:
 
             # Scaling factor is 365 unit-days / year
             afr_scaling_factor = 365.0
@@ -163,10 +116,11 @@ def _compute_afr_per_drive_model_family(
             annualized_failure_rate = (curr_datapoint[ 'cumulative_drive_failures' ] /
                                        curr_datapoint[ 'cumulative_drive_days' ]) * afr_scaling_factor
 
-            computed_afr_data[ curr_model_family_name ].append(
+            computed_afr_data[ curr_drive_model_name ].append(
                 {
-                    "day_index"     : curr_datapoint['day_index'],
                     "afr_percent"   : round(annualized_failure_rate * 100.0, 2),
+                    "date"          : curr_datapoint['date'],
+                    "day_index"     : curr_datapoint['day_index'],
                     #"cumulative_drive_failures" : curr_datapoint['cumulative_drive_failures'],
                     #"cumulative_drive_days"     : curr_datapoint['cumulative_drive_days'],
                 }
@@ -185,19 +139,21 @@ def _write_afr_data_to_csv(args: argparse.Namespace,
 
         # Write header row
         field_names: tuple[str, ...] = (
-            'drive_model_family',
+            'drive_model',
             'day_index',
+            'date',
             'annualized_failure_rate_percent',
         )
         csv_writer.writerow(field_names)
 
         # Deal with data rows
-        for curr_drive_model_family in sorted_drive_model_families:
-            for curr_afr_datapoint in drive_model_family_afr_data[curr_drive_model_family]:
+        for curr_drive_model in sorted_drive_model_families:
+            for curr_afr_datapoint in drive_model_family_afr_data[curr_drive_model]:
                 #print(json.dumps(curr_afr_datapoint))
                 current_row: tuple[str | int | float, ...] = (
-                        curr_drive_model_family,
+                        curr_drive_model,
                         curr_afr_datapoint[ 'day_index' ],
+                        curr_afr_datapoint[ 'date' ],
                         f"{curr_afr_datapoint[ 'afr_percent' ]:.02f}",
                     )
 
@@ -206,16 +162,14 @@ def _write_afr_data_to_csv(args: argparse.Namespace,
 
 def _main() -> None:
     args: argparse.Namespace = _parse_args()
-    drive_model_family_mappings: list[dict[str, str]] = _generate_regex_map(args)
-    parsed_csv_data: dict[str, dict[str, dict[str, int]]] = _parse_csv_data(
-        args, drive_model_family_mappings)
+    parsed_csv_data: dict[str, dict[str, dict[str, int]]] = _parse_csv_data(args)
     #print( json.dumps(parsed_csv_data, indent=4, sort_keys=True) )
 
     afr_prepped_data: dict[str, list[dict[str, int | str]]] = _prep_csv_data_for_afr_calc( parsed_csv_data )
 
-    drive_model_family_afr_data: dict[str, list[dict[str, int | str]]] = \
-        _compute_afr_per_drive_model_family(afr_prepped_data)
-    _write_afr_data_to_csv( args, drive_model_family_afr_data )
+    drive_model_afr_data: dict[str, list[dict[str, int | str]]] = \
+        _compute_afr_per_drive_model(afr_prepped_data)
+    _write_afr_data_to_csv( args, drive_model_afr_data )
 
 
 if __name__ == "__main__":
