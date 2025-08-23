f76f303c7ba2e95337e9d79ad505a3bc52b3f301
Terry Ott <Terry.Ott@gmail.com>
2025-08-23 22:34:36 +0000

    added readable name to drive count column out of Trino

--- retrieve_backblaze_drive_data.sh
+++ retrieve_backblaze_drive_data.sh
@@ -5,4 +5,4 @@ DOCKER_CONTAINER="backblaze-drive-stats-trino-iceberg-trino-1"
 TRINO_CATALOG="drivestats_b2"
 TRINO_DRIVESTATS_SCHEMA="ds_schema"
 
-${DOCKER} exec -it ${DOCKER_CONTAINER} trino --catalog ${TRINO_CATALOG} --schema ${TRINO_DRIVESTATS_SCHEMA} --execute "SELECT model, date, COUNT(serial_number), SUM(failure) AS failure_count FROM drivestats GROUP BY model, date ORDER BY model, date;" --output-format CSV_HEADER_UNQUOTED
+${DOCKER} exec -it ${DOCKER_CONTAINER} trino --catalog ${TRINO_CATALOG} --schema ${TRINO_DRIVESTATS_SCHEMA} --execute "SELECT model, date, COUNT(serial_number) AS drive_count, SUM(failure) AS failure_count FROM drivestats GROUP BY model, date ORDER BY model, date;" --output-format CSV_HEADER_UNQUOTED
