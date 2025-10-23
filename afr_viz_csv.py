20fc99fa8acd887999ea25156e35bac3a2a9fd03
Terry Ott <Terry.Ott@gmail.com>
2025-10-23 14:08:21 +0000

    Pulling 17t unique drive models in 10 seconds with 11 GB RAM

--- afr_viz_csv.py
+++ afr_viz_csv.py
@@ -9,16 +9,22 @@ def _parse_args() -> argparse.Namespace:
     parser.add_argument("parquet_file", help="Path to parquet file")
     return parser.parse_args()
 
+
 def _get_drive_model_mapping(original_source_lazyframe: polars.LazyFrame) -> dict[str, list[str]]:
     drive_model_mapping: dict[str, list[str]] = {}
 
     # We want all unique drive model names
-    drive_models_series: polars.Series = \
-        original_source_lazyframe.select("model").collect().to_series().unique().sort()
+    drive_models: polars.Series = \
+        original_source_lazyframe.select("model").collect().to_series().unique()
 
-    # Iterate over drive models in Series to create normalized name -> [raw names] mappings
-    for current_drive_model in drive_models_series:
+
+    # How many unique drive models?
+    #print( f"Got {len(drive_models):,} unique drive models" )
+
+    # Iterate over drive models to create normalized name -> [raw names] mappings
+    for current_drive_model in drive_models:
         print(f"Found drive model \"{current_drive_model}\"")
+
 
     return drive_model_mapping
 
@@ -31,3 +37,4 @@ def _main() -> None:
 
 if __name__ == "__main__":
     _main()
+
