59ab27f0f94fad3e3d88328f47bbbe4bc3b6f35f
Terry Ott <Terry.Ott@gmail.com>
2025-11-02 18:51:13 +0000

    Cleanup OBE experiments

--- retrieve_backblaze_drive_data_drive_distribution_over_time.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-DOCKER="/usr/bin/docker"
-DOCKER_CONTAINER="backblaze-drive-stats-trino-iceberg-trino-1"
-TRINO_CATALOG="drivestats_b2"
-TRINO_DRIVESTATS_SCHEMA="ds_schema"
-
-${DOCKER} exec -it ${DOCKER_CONTAINER} trino --catalog ${TRINO_CATALOG} --schema ${TRINO_DRIVESTATS_SCHEMA} \
---execute "SELECT date, model, count(model) AS model_count FROM drivestats GROUP by date, model HAVING model LIKE '%HUH72%' OR model LIKE '%WUH72%' OR model LIKE '%ST__000NM%' OR model LIKE '%MG__ACA__T%';" --output-format CSV_HEADER_UNQUOTED
