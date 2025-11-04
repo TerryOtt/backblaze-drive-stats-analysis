9cd58d248693fca37f78714516931083cf527244
Terry Ott <Terry.Ott@gmail.com>
2025-11-04 23:36:37 +0000

    Found the weird data, have an expensive fix, need to investigate 2018 Q1

--- quarterly_raw_storage_capacity.py
+++ quarterly_raw_storage_capacity.py
@@ -1,7 +1,8 @@
 import argparse
+import re
+import sys
 import time
 import polars
-from dateutil.rrule import YEARLY
 
 import backblaze_drive_stats_data
 import etl_pipeline
@@ -44,10 +45,106 @@ def _parse_args() -> argparse.Namespace:
     return parser.parse_args()
 
 
+# def _correct_source_lazyframe_drive_model_capacity(source_lazyframe: polars.LazyFrame) -> polars.LazyFrame:
+#
+#     drive_model_tb_corrections: dict[str, float] = {
+#
+#         r'^MTFDDAV240TCB$'                      :  0.240,
+#
+#         r'^CT250MX500SSD1$'                     :  0.250,
+#         r'\b250\s?GB$'                          :  0.250,
+#         r'^Seagate BarraCuda 120 SSD ZA250'     :  0.250,
+#         r'^Seagate IronWolf ZA250'              :  0.250,
+#         r'^WDC WDS250G'                         :  0.250,
+#         r'^Seagate BarraCuda SSD ZA250'         :  0.250,
+#
+#         r'^DELLBOSS VD$'                        :  0.480,
+#         r'^MTFDDAV480TCB$'                      :  0.480,
+#
+#         r'^Seagate BarraCuda 120 SSD ZA500'     :  0.500,
+#         r'^Seagate FireCuda 120 SSD ZA500'      :  0.500,
+#
+#         r'^WDC WD10E'                           :  1.000,
+#
+#         r'\bH[DM]S5C4040'                       :  4.001,
+#         r'\b[HW]U[HS]726040'                    :  4.001,
+#         r'^ST4000[A-Z]{2}'                      :  4.001,
+#         r'^TOSHIBA MD04ABA400V$'                :  4.001,
+#
+#         r'^ST6000[A-Z]{2}'                      :  6.001,
+#         r'^WDC WD60E'                           :  6.001,
+#
+#         r'\b[HW]U[HS]728080'                    :  8.002,
+#         r'\b[HW]U[HS]728T8T'                    :  8.002,
+#         r'^ST8000[A-Z]{2}'                      :  8.002,
+#         r'^TOSHIBA HDWF180$'                    :  8.002,
+#
+#         r'\b[HW]U[HS]721010'                    : 10.001,
+#         r'^ST10000[A-Z]{2}'                     : 10.001,
+#
+#         r'\b[HW]U[HS]721212'                    : 12.000,
+#         r'^ST12000[A-Z]{2}'                     : 12.000,
+#
+#         r'^ST14000[A-Z]{2}'                     : 14.001,
+#         r'^TOSHIBA MG\d{2}ACA14T'               : 14.001,
+#         r'\b[HW]U[HS]72\d{2}14'                 : 14.001,
+#
+#         r'^ST16000[A-Z]{2}'                     : 16.001,
+#         r'^TOSHIBA MG\d{2}ACA16T'               : 16.001,
+#         r'\b[HW]U[HS]72\d{2}16'                 : 16.001,
+#
+#         r'^ST18000[A-Z]{2}'                     : 18.000,
+#
+#         r'^TOSHIBA MG\d{2}ACA20T'               : 20.001,
+#
+#         r'\b[HW]U[HS]72\d{2}2'                  : 22.001,
+#
+#         r'^ST24000[A-Z]{2}'                     : 24.000,
+#     }
+#
+#     # Get all the models with sizes that aren't real
+#     bad_boy_models: list[str] = source_lazyframe.filter(
+#         # LT 1 GB                                             GT 40 TB
+#         polars.col("capacity_bytes").lt(1000 * 1000 * 1000) | polars.col("capacity_tb").gt(40.0)
+#     ).select(
+#         "model_name",
+#     ).unique().collect().get_column("model_name").to_list()
+#
+#     # Create size correction lists
+#     update_model_name: list[str] = []
+#     update_capacity_tb: list[float] = []
+#     for curr_bad_boy in bad_boy_models:
+#         found_capacity: bool = False
+#         for match_regex, match_capacity_tb in drive_model_tb_corrections.items():
+#             if re.search(match_regex, curr_bad_boy):
+#                 update_model_name.append(curr_bad_boy)
+#                 update_capacity_tb.append(match_capacity_tb)
+#                 found_capacity = True
+#                 break
+#
+#         # If we didn't find a mapping, blow up the world
+#         if not found_capacity:
+#             raise ValueError(f"Could not find correct capacity for model {curr_bad_boy}")
+#
+#     corrected_data_lazyframe: polars.LazyFrame = polars.LazyFrame(
+#         {
+#             "model_name"    : update_model_name,
+#             "capacity_tb"   : update_capacity_tb,
+#         }
+#     )
+#
+#     updated_lazyframe: polars.LazyFrame = source_lazyframe.update(
+#         corrected_data_lazyframe,
+#         on="model_name",
+#         how="full",
+#     )
+#
+#     return updated_lazyframe
+
+
 def _get_source_lazyframe(args: argparse.Namespace) -> polars.LazyFrame:
     source_lazyframe: polars.LazyFrame = backblaze_drive_stats_data.source_lazyframe(args)
 
-    # Get
     print( etl_pipeline.next_stage_banner() )
 
                           # KB     MB     GB     TB
@@ -57,10 +154,19 @@ def _get_source_lazyframe(args: argparse.Namespace) -> polars.LazyFrame:
     source_lazyframe = source_lazyframe.select(
         "date",
         polars.col("model").alias("model_name"),
+        "capacity_bytes",
         (polars.col("capacity_bytes") / bytes_per_tb).alias("capacity_tb"),
         "serial_number",
+
+    # Remove rows with bad drive capacity data reported by SMART
+    ).filter(
+        polars.col("capacity_tb").is_between(0.001, 25.000)
     )
     # print(source_lazyframe.collect_schema())
+
+    # Add logic so data streamed through the dataframe has any bunk byte capacities fixed
+    # source_lazyframe = _correct_source_lazyframe_drive_model_capacity(source_lazyframe)
+
     print("\tCompleted")
 
     return source_lazyframe
@@ -70,15 +176,6 @@ def _get_materialized_quarterly_storage_capacity(source_lazyframe: polars.LazyFr
     print(etl_pipeline.next_stage_banner())
     stage_begin: float = time.perf_counter()
     print("\tMaterializing quarterly raw storage capacity from Apache Iceberg on Backblaze B2 using Polars...")
-
-    # # Start with creating a dataframe mapping drive model -> capacity in TB
-    # unique_models_with_capacity_tb: polars.DataFrame = source_lazyframe.unique(
-    #     "model_name"
-    # ).select(
-    #     "model_name", "capacity_tb"
-    # ).collect()
-
-    #print(unique_models_with_capacity_tb)
 
     tb_per_pb: float = 1000.0
     pb_per_eb: float = 1000.0
@@ -119,11 +216,13 @@ def _main() -> None:
 
     etl_pipeline.create_pipeline(
         (
-            "Get source lazyframe",
+            "Get source lazyframe (note: includes to fix rows with invalid capacities from SMART",
             "Create quarterly raw storage capacity data",
         )
     )
 
+    # Note: any models read with incorrect capacities will be corrected by the logic in this source
+    #       lazyframe
     source_lazyframe: polars.LazyFrame = _get_source_lazyframe(args)
 
     quarterly_raw_storage_capacity_dataframe: polars.DataFrame = _get_materialized_quarterly_storage_capacity(
@@ -131,16 +230,13 @@ def _main() -> None:
 
     print("\nBackblaze Raw Storage Capacity By Quarter:\n")
 
-    # Go find what's wrong with
-    #   - 2014 Q1
-    #   - 2015 Q2
-    #   - 2018 Q1
+    for curr_row in quarterly_raw_storage_capacity_dataframe.iter_rows():
+        year, quarter, raw_capacity_pb, raw_capacity_eb = curr_row
 
-    # for curr_row in quarterly_raw_storage_capacity_dataframe.iter_rows():
-    #     year, quarter, raw_capacity_pb, raw_capacity_eb = curr_row
-    #
-    #     print(f"\t{year} Q{quarter}: {raw_capacity_pb:6,.0f} petabytes (PB) / "
-    #           f"{raw_capacity_eb:4.01f} exabytes (EB)")
+        print(f"\t{year} Q{quarter}: {raw_capacity_pb:6,.0f} petabytes (PB) / "
+              f"{raw_capacity_eb:4.01f} exabytes (EB)")
+
+    raise NotImplementedError("TODO: need to investigate 2018 Q1 with twice the storage it should have")
 
 
 if __name__ == "__main__":
