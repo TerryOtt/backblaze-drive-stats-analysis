0be8a305be8473702d77e28928b66fa2da0e7a76
Terry Ott <Terry.Ott@sixbuckssolutions.com>
2026-06-14 20:15:17 +0000

    More and more upgrade

--- install-latest-python-env.sh
+++ install-latest-python-env.sh
@@ -16,13 +16,13 @@ ${ECHO} "  - Removing Python virtual environment if there is one"
 ${RM} -rf ./.venv
 
 # Update uv to latest Python interpreter that's >= 3.14.0
-${ECHO} "  - Upgrading uv Python to latest release that's >= 3.14.0"
+${ECHO} "  - Installing/upgrading uv Python to latest stable >= 3.14.0"
 ${UV} python upgrade --quiet ">=3.14"
 
 # Update pyproject.toml to latest version of all Python deps
-${ECHO} "  - Updating pyproject.toml with latest released versions of all dependencies"
+${ECHO} "  - Updating pyproject.toml with latest stable versions of all dependencies"
 ${UV} lock --upgrade --quiet
 
 # Install newly-refreshed list of dep versions
-${ECHO} "  - Installing refreshed list of Python package dependencies"
+${ECHO} "  - Installing all dependencies in the newly-refreshed pyproject.toml"
 ${UV} sync --quiet
