d5622fe89a7754865915a33962b9f614e4312226
Terry Ott <Terry.Ott@sixbuckssolutions.com>
2026-06-14 21:06:14 +0000

    Doing a full wipe of all managed versions and then installing latest stable >=3.14

--- install-latest-python-env.sh
+++ install-latest-python-env.sh
@@ -15,9 +15,13 @@ ${RM} -rf ~/.cache/uv
 ${ECHO} "  - Removing Python virtual environment if there is one"
 ${RM} -rf ./.venv
 
+# Remove all managed (i.e., non-managed) uv Python installs
+${ECHO} "  - Uninstalling all managed uv Python interpreters"
+${UV} python uninstall --all --quiet
+
 # Update uv to latest Python interpreter that's >= 3.14.0
-${ECHO} "  - Installing/upgrading uv Python to latest stable >= 3.14.0"
-${UV} python upgrade --quiet ">=3.14"
+${ECHO} "  - Installing uv Python as latest stable >= 3.14.0"
+${UV} python install --quiet ">=3.14"
 
 # Update uv.lock with latest version of all Python deps
 ${ECHO} "  - Updating uv.lock with latest stable versions of all dependencies"
